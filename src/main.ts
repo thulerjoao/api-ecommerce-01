e5f8d470901dea8a4ae00330fc6ceba2175a6628
João Pedro Thuler Lima <thuler_lima@hotmail.com>
2022-09-14 16:58:18 -0300

    config: instaled and configured the initial setup

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  const config = new DocumentBuilder()
+    .setTitle('API ecommerce 01')
+    .setDescription('API for ecommerce based on the first bootcamp')
+    .setVersion('1.0.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3333);
+}
+bootstrap();
